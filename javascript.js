cd6ede95d199d057e8d34a461c4b9a5713f66cf8
williambeach <intratech.cs@icloud.com>
2022-05-17 15:26:52 -0400

    modify css to contain child elements if zoom in or out. Add change grid number button

--- javascript.js
+++ javascript.js
@@ -1,25 +1,56 @@
 function add16X16Grids() {
-    container.innerHTML = "";
     container = document.getElementById("container");
-    childDiv = document.createElement("div");
+    width = (576 / 16);
+    height = (576 / 16);
     for (let i = 0; i < 256; i++) {
         childDiv = document.createElement("div");
-        childDiv.classList.add("grid16x16");
+        childDiv.classList.add("grid");
+        childDiv.style.width = `${width}px`;
+        childDiv.style.height = `${height}px`;
         container.append(childDiv);
     }
 }
 
 
-function addCustomGrids() {
-    container.innerHTML = "";
+
+function addCustomGrids(value) {
     container = document.getElementById("container");
-    childDiv = document.createElement("div");
-    for (let i = 0; i < 1024; i++) {//i < userInput via popup window text input <= 100.
+    container.innerHTML = "";
+    width = (576 / value);
+    height = (576 / value);
+    totalSquares = value * value;
+    for (let i = 0; i < totalSquares; i++) {
         childDiv = document.createElement("div");
-        childDiv.classList.add("grid32x32");//style with javascript related to size inputted
+        childDiv.classList.add("grid");
+        childDiv.style.width = `${width}px`;
+        childDiv.style.height = `${height}px`;
         container.append(childDiv);
     }
 }
+
+function popUpWindow() {
+    Swal.fire({
+        title: 'How many grids per side?',
+        icon: 'question',
+        input: 'range',
+        inputLabel: 'Number of grids',
+        inputAttributes: {
+          min: 16,
+          max: 100,
+          step: 1
+        }
+    }).then((result) => {
+        if (result.value) {
+            addCustomGrids(result.value);
+        }
+    });
+}
+
+function gridButtonClicked() {
+    gridButton = document.getElementById("gridButton");
+    gridButton.addEventListener("click", popUpWindow);
+}
+
 
 add16X16Grids();
-addCustomGrids();
+gridButtonClicked();
