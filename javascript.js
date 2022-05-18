cf48d206f388de1b56b5c47c181e50ce3528077b
williambeach <intratech.cs@icloud.com>
2022-05-18 15:48:30 -0400

    add random color hover effect and button for changing grid quantity

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
 function add16X16Grids() {
     container = document.getElementById("container");
-    width = (850 / 16);
-    height = (640 / 16);
+    width = (750 / 16);
+    height = (500 / 16);
     for (let i = 0; i < 256; i++) {
         childDiv = document.createElement("div");
         childDiv.classList.add("grid");
@@ -9,15 +9,26 @@ function add16X16Grids() {
         childDiv.style.height = `${height}px`;
         container.append(childDiv);
     }
+    grid = document.querySelectorAll(".grid");
+    for (let i=0; i<grid.length;i++) {
+        grid[i].addEventListener("mouseover", () => {
+            button = document.querySelector("#rainbowMode");
+            if (button.value == "OFF") {
+                grid[i].style.backgroundColor = "black";
+            } else {
+                randomColor = randomHsl();
+                grid[i].style.backgroundColor = randomColor;
+            }
+        });
+    }
 }
-
 
 
 function addCustomGrids(value) {
     container = document.getElementById("container");
     container.innerHTML = "";
-    width = (576 / value);
-    height = (576 / value);
+    width = (750 / value);
+    height = (500 / value);
     totalSquares = value * value;
     for (let i = 0; i < totalSquares; i++) {
         childDiv = document.createElement("div");
@@ -25,6 +36,18 @@ function addCustomGrids(value) {
         childDiv.style.width = `${width}px`;
         childDiv.style.height = `${height}px`;
         container.append(childDiv);
+    }
+    grid = document.querySelectorAll(".grid");
+    for (let i=0; i<grid.length;i++) {
+        grid[i].addEventListener("mouseover", () => {
+            button = document.querySelector("#rainbowMode");
+            if (button.value == "OFF") {
+                grid[i].style.backgroundColor = "black";
+            } else {
+                randomColor = randomHsl();
+                grid[i].style.backgroundColor = randomColor;
+            }
+        });
     }
 }
 
@@ -50,6 +73,25 @@ function gridButtonClicked() {
     gridButton = document.getElementById("gridButton");
     gridButton.addEventListener("click", popUpWindow);
 }
+
+function toggle(button) {
+    if (button.value == "OFF") {
+      button.value = "ON";
+      button.textContent = "Ink";
+    } else {
+      button.value = "OFF";
+      button.textContent = "Rainbow";
+    }
+  }
+
+
+function randomHsl() {
+    return 'hsla(' + (Math.random() * 360) + ', 100%, 50%, 1)';
+}
+
+
+
+
 
 
 add16X16Grids();
